d50a27c31be40583544b712870f61a0e818bb90f
AntonR8 <129757394+AntonR8@users.noreply.github.com>
2024-08-30 14:52:24 +0300

    Initial Commit

---
+++ EffectiveMobileToDoList/EffectiveMobileToDoListApp.swift
@@ -0,0 +1,17 @@
+//
+//  EffectiveMobileToDoListApp.swift
+//  EffectiveMobileToDoList
+//
+//  Created by Антон Разгуляев on 30.08.2024.
+//
+
+import SwiftUI
+
+@main
+struct EffectiveMobileToDoListApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
